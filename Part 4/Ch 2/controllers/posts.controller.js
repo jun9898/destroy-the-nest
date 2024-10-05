0c625b799abaf4545fdb3ebe3a33e1b048dda5df
jun9898 <nice1998@gmail.com>
2024-10-05 12:40:39 +0900

    node 기초 수강 완료

--- Part 4/Ch 2/controllers/posts.controller.js
+++ Part 4/Ch 2/controllers/posts.controller.js
@@ -1,6 +1,12 @@
+const path = require('path');
 function getPost(req, res) {
-    res.send('<div><h1>Post Title</h1><p>This is a post</p></div>');
+    res.render('posts', {
+        templateName: 'post',
+    });
+    // res.send('<div><h1>Post Title</h1><p>This is a post</p></div>');
+    // res.sendFile(path.join(__dirname, "..", "public", "images", "514a59e095e1d0d4b2512729844fd3a4.jpg"));
 }
+
 
 module.exports = {
     getPost,
--- Part 4/Ch 2/server.js
+++ Part 4/Ch 2/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require("path");
 const usersController = require('./controllers/users.controller');
 const postsController = require('./controllers/posts.controller');
 
 const PORT = 3000;
 
 const app = express();
+
+const usersRouter = require('./routes/users.router');
+const postsRouter = require('./routes/posts.router');
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
@@ -13,20 +22,23 @@ app.use((req, res, next) => {
     console.log(`start : ${req.method} ${req.url}`);
     next();
     const diffTime = Date.now() - start;
-    console.log(`end : ${req.method} ${req.url} ${diffTime}ms`);
+    console.log(`end : ${req.method} ${req.baseUrl} ${req.url} ${diffTime}ms`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render('index', {
+        imageTitle: 'it is a penguin',
+    });
 });
 
-// User
-app.get('/users', usersController.getUsers);
-app.get('/users/:userId', usersController.getUser);
-app.post('/users', usersController.postUser);
+app.get('/', (req, res) => {
+    res.render('posts', {
+        templateName: 'post',
+    });
+});
 
-// Post
-app.get("/posts", postsController.getPost);
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
