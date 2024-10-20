5891817fd6d28474b86c37a0d75036fccbc9fcba
jun9898 <nice1998@gmail.com>
2024-10-20 16:54:18 +0900

    Part 9 Post page 구현중

--- Part 9/express-passport-email-app/src/routes/posts.router.js
+++ Part 9/express-passport-email-app/src/routes/posts.router.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const {checkAuthenticated} = require("../middlewares/auth");
 const router = express.Router();
+const Post = require('../models/posts.model');
 
-router.get('/', (req, res) => {
-    res.render('posts');
+router.get('/', checkAuthenticated, (req, res) => {
+    // populate는 join과 비슷한 역할을 한다.
+    Post.find()
+        .populate('comments')
+        .sort({ createdAt: 'desc' })
+        .exec()
+        .then(posts => {
+            res.render('posts', {
+                posts: posts,
+                currentUser: req.user
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
 });
 
 module.exports = router;
