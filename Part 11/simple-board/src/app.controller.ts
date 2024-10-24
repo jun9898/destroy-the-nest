6c00051053b7f4f395fd9fc0f4f5415fc4d1e3a8
jun9898 <nice1998@gmail.com>
2024-10-24 23:23:03 +0900

    Part 11 Nest 프로젝트 세팅 완료

---
+++ Part 11/simple-board/src/app.controller.ts
@@ -0,0 +1,26 @@
+import {Controller, Get, Param, Query} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('name/:name')
+  getName(
+      @Param('name') name: string
+  ): string {
+    return `${name} hello`
+  }
+
+  @Get('name')
+  getQueryName(
+      @Query('name') name: string
+  ): string {
+    return `${name} hello`
+  }
+}
