e2ef9ef0fe3a18c53a57f703b33bed959e66b2fc
jun9898 <nice1998@gmail.com>
2024-10-25 23:40:05 +0900

    Part 11 passport 로그인 기능 구현

---
+++ Part 11/simple-board/src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import {Module} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "../entities/user.entity";
+import {UserModule} from "../routes/user/user.module";
+import {LocalStrategy} from "./auth.strategy";
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
+import {config} from "dotenv";
+import * as process from "node:process";
+import {JwtStrategy} from "./jwt-strategy";
+config({path: '.env.local'});
+
+@Module({
+  imports: [
+      UserModule,
+      PassportModule,
+      TypeOrmModule.forFeature([User]),
+      JwtModule.register({
+          secret: process.env.JWT_SECRET,
+          signOptions: {expiresIn: '1h'},
+      }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ Part 11/simple-board/src/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import {UserService} from "../routes/user/user.service";
+import {compare} from 'bcrypt';
+import {User} from "../entities/user.entity";
+import {JwtService} from "@nestjs/jwt";
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) {}
+
+    async validateUser(username: string, password: string) {
+        const user = await this.userService.getUserByUsername(username);
+
+        // passport 에서는 null을 리턴하면 인증 실패로 간주
+        if (!user) return null;
+
+        if (await compare(password, user.password)) {
+            return user;
+        } else {
+            return null;
+        }
+    }
+
+    async login(user: User) {
+
+        const payload = {
+            username: user.username,
+            name: user.name,
+        };
+
+        return {
+            accessToken: this.jwtService.sign(payload, {expiresIn: '1d'}),
+        };
+    }
+}
